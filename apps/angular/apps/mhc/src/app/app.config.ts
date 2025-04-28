dbfb6ccda3f1cf8ccb2ef69cbbcc0668c1d20461
andrew <dev@madhareconsulting.com>
2025-04-27 22:47:59 -0400

    COMMIT TO MAIN

--- apps/angular/apps/mhc/src/app/app.config.ts
+++ apps/angular/apps/mhc/src/app/app.config.ts
@@ -5,11 +5,25 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    providePrimeNG({
+      theme: {
+          preset: Aura,
+          options: {
+              cssLayer: {
+                  name: 'primeng',
+                  order: 'tailwind-base, primeng, tailwind-utilities'
+              }
+          }
+      }
+  })
+
   ],
 };
