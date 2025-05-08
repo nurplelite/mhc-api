43a782eb78eb4f243302eea6d13e772a4831a1ec
andrew <dev@madhareconsulting.com>
2025-05-07 23:01:54 -0400

    gmail service added

---
+++ libs/shared/gmail/README.md
@@ -0,0 +1,7 @@
+# gmail
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build gmail` to build the library.
---
+++ libs/shared/gmail/src/lib/gmail.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { google } from 'googleapis';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class GmailService {}
