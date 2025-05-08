1ea36be2a2741eb100da07f4ce848c392a14a4bc
andrew <dev@madhareconsulting.com>
2025-05-08 03:57:45 -0400

    added gmail service in shared library

--- libs/shared/gmail/src/lib/gmail.service.ts
+++ libs/shared/gmail/src/lib/gmail.service.ts
@@ -1,6 +1,37 @@
-import { Injectable } from '@nestjs/common';
 import { google } from 'googleapis';
-import { ConfigService } from '@nestjs/config';
+import { JWT } from 'google-auth-library';
 
-@Injectable()
-export class GmailService {}
+const SCOPES = ['https://mail.google.com/'];
+
+export class GmailService {
+  private jwtClient: JWT;
+
+  constructor(private delegatedUser: string) {
+    this.jwtClient = new google.auth.JWT({
+      email: process.env.GSA_CLIENT_EMAIL, // service account email
+      key: process.env.GSA_PRIVATE_KEY?.replace(/\\n/g, '\n'), // properly formatted private key
+      scopes: SCOPES,
+      subject: delegatedUser, // sendAs user
+    });
+  }
+
+  async sendMail({ to, subject, message }: { to: string, subject: string, message: string }) {
+    const gmail = google.gmail({ version: 'v1', auth: this.jwtClient });
+
+    const rawMessage = Buffer.from([
+      `From: ${this.delegatedUser}`,
+      `To: ${to}`,
+      `Subject: ${subject}`,
+      'Content-Type: text/html; charset=UTF-8',
+      '',
+      message,
+    ].join('\n')).toString('base64url');
+
+    await gmail.users.messages.send({
+      userId: 'me',
+      requestBody: {
+        raw: rawMessage,
+      },
+    });
+  }
+}
