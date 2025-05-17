daaf93f0046aacad4133917dd2c3fb099b3b7bca
andrew <dev@madhareconsulting.com>
2025-05-17 10:44:19 -0400

    new app created for main site, added material, and ssr, needs tailwind

--- apps/angular/apps/madhare/src/app/app.component.ts
+++ apps/angular/apps/madhare/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButton } from '@angular/material/button';
+
+
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [ RouterModule, MatGridListModule, MatButton ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
