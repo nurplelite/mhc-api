6941b06b36ea175c14ac79b9cfc30736eaec469d
andrew <dev@madhareconsulting.com>
2025-05-19 12:52:37 -0400

    added components for landing page

---
+++ apps/angular/app/madhare/src/app/components/features.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'features',
+  imports: [CommonModule],
+  template: `<p>features works!</p>`,
+  styles: ``,
+})
+export class FeaturesComponent {}
---
+++ apps/angular/app/madhare/src/app/components/hero.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'hero',
+  imports: [CommonModule],
+  template: `<p>hero works!</p>`,
+  styles: ``,
+})
+export class HeroComponent {}
