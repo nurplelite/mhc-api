d5ae292611d21971f7da5d51e188cc9c2a6a31b0
andrew <dev@madhareconsulting.com>
2025-05-21 08:57:07 -0400

    changed to libs/api

--- apps/mhc-api/src/app/modules/site-ssr/site-ssr.service.ts
+++ apps/mhc-api/src/app/modules/site-ssr/site-ssr.service.ts
@@ -4,7 +4,18 @@ import { UpdateSiteSsrDto } from './dto/update-site-ssr.dto';
 
 @Injectable()
 export class SiteSsrService {
+  private siteId: string
 
+
+  // Get siteId from the constructor to initialize site
+
+  constructor(siteId: string) {
+    this.siteId = siteId
+  }
+
+  siteConfig(this.siteId){
+
+  }
   render(createSiteSsrDto: CreateSiteSsrDto) {
     return 'This returns the static html';
   }
